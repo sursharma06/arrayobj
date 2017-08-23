de470adb7fe8da072713dd0ea823b2784d2c6fad
Surbhi Sharma <sharma.surbhi13@gmail.com>
2017-08-23 14:19:12 -0400

    objects added

--- array.js
+++ array.js
@@ -9,3 +9,15 @@ myArray.push(20);
 
 var colorsArray = ['red', 'blue', 'white', 'pink', 'black', 'purple'];
 colorsArray.pop();
+
+var user1 = {
+  username: 'sursharma06',
+  password: '1234',
+};
+
+var user2 = {
+  username: 'bob',
+  password: '3456',
+};
+
+user1.username; //using dot operator with objects to access values.
